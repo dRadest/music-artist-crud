80c2c35da70899e17b7c2449732f9bba619fdec3
drad <progdrad@gmail.com>
2024-04-14 09:11:20 +0200

    Init the project

---
+++ src/main/java/com/dradest/music/artist/crud/MusicArtistCrudApplication.java
@@ -0,0 +1,13 @@
+package com.dradest.music.artist.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicArtistCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicArtistCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dradest/music/artist/crud/MusicArtistCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dradest.music.artist.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicArtistCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
