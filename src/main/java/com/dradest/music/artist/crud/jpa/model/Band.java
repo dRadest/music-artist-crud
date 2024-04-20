86d30d8a579e9062dd9d78fc619eb985f069974b
drad <progdrad@gmail.com>
2024-04-20 15:11:48 +0200

    Show band details in a dialog

--- src/main/java/com/dradest/music/artist/crud/jpa/model/Band.java
+++ src/main/java/com/dradest/music/artist/crud/jpa/model/Band.java
@@ -3,6 +3,7 @@ package com.dradest.music.artist.crud.jpa.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity(name = "band")
@@ -20,13 +21,14 @@ public class Band {
             fetch = FetchType.EAGER // FIXME: set fetch to LAZY and load entities where needed
     )
     // technically, an artist could belong to more than one band, but we won't cover that case (it would require a ManyToMany mapping)
-    private Set<Artist> members;
+    private Set<Artist> members = new HashSet<>();
     @OneToMany(
             mappedBy = "band",
             cascade = CascadeType.ALL,
-            orphanRemoval = true
+            orphanRemoval = true,
+            fetch = FetchType.EAGER
     )
-    private Set<Album> albums;
+    private Set<Album> albums = new HashSet<>();
 
 
     public Long getId() {
--- src/main/java/com/dradest/music/artist/crud/ui/views/band/BandView.java
+++ src/main/java/com/dradest/music/artist/crud/ui/views/band/BandView.java
@@ -1,17 +1,25 @@
 package com.dradest.music.artist.crud.ui.views.band;
 
+import com.dradest.music.artist.crud.jpa.model.Artist;
 import com.dradest.music.artist.crud.jpa.model.Band;
 import com.dradest.music.artist.crud.jpa.persistence.CrudService;
 import com.dradest.music.artist.crud.ui.views.MainLayout;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.renderer.NativeButtonRenderer;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
+import java.time.format.DateTimeFormatter;
+import java.util.StringJoiner;
 
 @Route(value = "bands", layout = MainLayout.class)
 @PageTitle("Bands")
@@ -37,10 +45,74 @@ public class BandView extends VerticalLayout {
         grid.addClassNames("band-grid");
         grid.setSizeFull();
         grid.setColumns("name");
+        grid.addColumn(
+                new NativeButtonRenderer<>("Details",
+                        clickedBand -> {
+                            Dialog dialog = createBandDetailsDialog(clickedBand);
+                            dialog.open();
+                        })
+        );
         grid.getColumns().forEach(col -> col.setAutoWidth(true));
 
         grid.asSingleSelect().addValueChangeListener(event ->
                 editBand(event.getValue()));
+    }
+
+    private Dialog createBandDetailsDialog(Band band) {
+        Dialog dialog = new Dialog();
+
+        dialog.setHeaderTitle(band.getName());
+
+        VerticalLayout dialogLayout = createDialogLayout(band);
+        dialog.add(dialogLayout);
+
+        Button cancelButton = new Button("Ok", e -> dialog.close());
+        dialog.getFooter().add(cancelButton);
+        return dialog;
+    }
+
+    private static VerticalLayout createDialogLayout(Band band) {
+        TextArea membersTextArea = new TextArea("Members");
+        TextArea albumsTextArea = new TextArea("Albums");
+
+        membersTextArea.setValue(constructArtistDetails(band));
+        membersTextArea.setReadOnly(true);
+
+        albumsTextArea.setValue(constructAlbumDetails(band));
+        albumsTextArea.setReadOnly(true);
+
+        VerticalLayout dialogLayout = new VerticalLayout(membersTextArea, albumsTextArea);
+        dialogLayout.setPadding(false);
+        dialogLayout.setSpacing(false);
+        dialogLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
+        dialogLayout.getStyle().set("width", "18rem").set("max-width", "100%");
+
+        return dialogLayout;
+    }
+
+    private static String constructArtistDetails(Band band) {
+        StringJoiner stringJoiner = new StringJoiner("\n");
+        band.getMembers().forEach(artist -> {
+            String formattedArtist = String.format("%s %s (%s)",
+                    artist.getFirstName(),
+                    artist.getLastName(),
+                    artist.getCountry() != null ? artist.getCountry().getCode() : "NA");
+            stringJoiner.add(formattedArtist);
+        });
+
+        return stringJoiner.toString();
+    }
+
+    private static String constructAlbumDetails(Band band) {
+        StringJoiner stringJoiner = new StringJoiner("\n");
+        band.getAlbums().forEach(album -> {
+            String formattedArtist = String.format("%s (%s)",
+                    album.getTitle(),
+                    album.getReleaseDate() != null ? album.getReleaseDate().format(DateTimeFormatter.ofPattern("yyyy")) : "NA");
+            stringJoiner.add(formattedArtist);
+        });
+
+        return stringJoiner.toString();
     }
 
     private void configureForm() {
